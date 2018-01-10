e5201b7e7e90fa11db8ffd80708001113c600a49
Michał Bieda <michal.bieda@gmail.com>
2018-01-10 16:15:19 +0100

    Add log and executable file to gitignore

--- pressed.sh
+++ pressed.sh
@@ -6,3 +6,4 @@ export DISPLAY=:0
 WID=$(xdotool search --onlyvisible --class chromium|head -1)
 xdotool windowactivate ${WID}
 xdotool key p
+
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 ./buttons > run.log
+
